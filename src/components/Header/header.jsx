2f65cff7588ab24610815b6b9490af6efef6418f
herbertdantas <herbertbezerradasilva@gmail.com>
2023-03-17 20:54:33 -0300

    criando e estilizando componente header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import dncLogo from '../../assets/logo-dnc.png'
+import './index.scss'
 
 const header = () => {
   return (
-    <div>
-        
+    <div className="header">
+      <img className="header__img" src={dncLogo} alt="dnc logo" />
     </div>
   )
 }
