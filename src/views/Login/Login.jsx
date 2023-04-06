76db9d8cd494b0c707e6345ead1fe9c568102bb1
marcelosifr <118279059+marcelosifr@users.noreply.github.com>
2023-04-06 20:33:09 -0300

    Atualização bug.
    Atualizando "bug".

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import "./index.css";
-//import Header from "../../components/Header/Header";
+import "./index.scss";
+import Header from "../../components/Header/Header";
 
 const login = () => {
   return (
 
     <section className='login'>
-        {/* <Header /> */}
+        <Header />
         <form className='login__form'>
             <h1>Acesse com seu login ou cadastre-se!</h1>
             <h2>você pode entrar com seu CPF.</h2>
